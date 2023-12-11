0ec1912c5596ced9dffa71779f7d9864d03a0576
Gilbert <shikweg@gmail.com>
2023-12-11 22:55:17 +0300

    Failed one test

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+let greeting = ""
+ if (feet <= 400) {
+  greeting = "This one is on me!"
+  return greeting
+  }    
+    else 
+        if (feet > 2000){
+          greeting = "I will gladly take your thirty bucks."
+          return greeting
+    
+}   else 
+       if (feet > 2500){
+       greeting = "No can do."
+       return greeting      
+}   else {
+       return "none"
+}
+}
+function ternaryCheckCity(city){
+  // Write your code here!
+  return (city == "NYC")? "Ok, sounds good." : "No go.";
+  
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(message){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (message){
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    case "thanks for everything":
+      return "Bye."
+      break;
+  }
 }
